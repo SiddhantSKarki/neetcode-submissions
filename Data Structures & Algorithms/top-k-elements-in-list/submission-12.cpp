f719ef53991613cffc4ba960166024af19a07242
Sid <108712314+SiddhantSKarki@users.noreply.github.com>
2026-09-25 16:37:29 -0700

    Add: top-k-elements-in-list - submission-12

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-12.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        if (nums.size() <= 1) {
+            return nums;
+        }
+        vector<int> output;
+
+        // get freq of numbers
+        unordered_map<int, int> num_freq;
+        for (int& num: nums) {
+            num_freq[num]++;
+        }
+
+        // get reverse mapping
+        // [frequency] -> [what elements in this frequency]
+        vector<vector<int>> freq_num(nums.size() + 1);
+        for (auto& pr: num_freq) {
+            freq_num[pr.second].push_back(pr.first);
+        }
+
+        // iterate through max possible size of freq
+        for (int idx = freq_num.size() - 1; idx > 0; --idx) {
+            for (int num: freq_num[idx]) {
+                output.push_back(num);
+                if (output.size() == k) {
+                    return output;
+                }
+            }
+        }
+        return output;
+    }
+};
